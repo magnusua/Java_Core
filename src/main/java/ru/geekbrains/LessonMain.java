52c4578b53939bcf4df1aad3bd4837244744aa7d
magnusua <uscmagnus@gmail.com>
2020-11-29 11:32:37 +0200

    Initial commit

---
+++ src/main/java/ru/geekbrains/LessonMain.java
@@ -0,0 +1,18 @@
+package ru.geekbrains;
+import java.util.*;
+
+public class LessonMain {
+    public static final Scanner scanner = new Scanner(System.in);
+    public static Random random = new Random();
+
+    public static void print(String text) {
+        System.out.println(text);
+        System.out.print(System.lineSeparator());
+    }
+
+    public static void main(String[] args) { // начало main
+
+        
+
+    } //конец main
+}
