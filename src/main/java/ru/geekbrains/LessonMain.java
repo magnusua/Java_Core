d2d41385944c84b7599b07e918a0999c70c7cb2f
magnusua <uscmagnus@gmail.com>
2020-12-03 22:41:54 +0200

    1. Напишите метод, на вход которого подается двумерный строковый массив размером 4х4, при подаче массива другого размера необходимо бросить исключение MyArraySizeException. 2. Далее метод должен пройтись по всем элементам массива, преобразовать в int, и просуммировать. Если в каком-то элементе массива преобразование не удалось (например, в ячейке лежит символ или текст вместо числа), должно быть брошено исключение MyArrayDataException – с детализацией, в какой именно ячейке лежат неверные данные. 3. В методе main() вызвать полученный метод, обработать возможные исключения MySizeArrayException и MyArrayDataException и вывести результат расчета.

--- src/main/java/ru/geekbrains/LessonMain.java
+++ src/main/java/ru/geekbrains/LessonMain.java
@@ -13,58 +13,78 @@ public class LessonMain {
         System.out.print(System.lineSeparator());
     }
 
-    public static void main(String[] args) { // начало main
-        print("Введите число участников, или нажмите ввод для 5 участников, введите число для заполнения или 0 для выхода.");
-        int input_answer = Math.abs(NumberUtils.toInt((scanner.nextLine()), 5));
-        if (!(input_answer == 0)) {
-            //генерируем участников
-            ArrayList<Subject> allSubject = new ArrayList<Subject>();
-            for (int i = 0; i < input_answer; i++) {
-                int Type = random.nextInt(3);
-                if (Type == 0) {
-                    allSubject.add(new Cat(RandomName("toxy"), random.nextInt(500), random.nextInt(200)));
-                }
-                if (Type == 1) {
-                    allSubject.add(new Human(RandomName("yan'kus'"), random.nextInt(500), random.nextInt(200)));
-                }
-                if (Type == 2) {
-                    allSubject.add(new Robot(RandomName("ited"), random.nextInt(500), random.nextInt(800)));
-                }
-            }
-            //генерируем препятствия
-            print("Введите число препятствий, или нажмите ввод для 2 препятствий.");
-            input_answer = Math.abs(NumberUtils.toInt((scanner.nextLine()), 2));
-            ArrayList<Object> allObject = new ArrayList<Object>();
-            for (int i = 0; i < input_answer; i++) {
-                int Type = random.nextInt(2);
-                if (Type == 0) {
-                    allObject.add(new Distance(random.nextInt(500)));
-                }
-                if (Type == 1) {
-                    allObject.add(new Wall(random.nextInt(200)));
-                }
-            }
+    public static void main(String[] args) throws MyArraySizeException, MyArrayDataException { // начало main
+        print("Help, it alive! Press Enter for start default." + "\r\n" + "Введите размер массива отличный от 4 или 0 для выхода");
+        int input_answer = Math.abs(NumberUtils.toInt((scanner.nextLine()), 4));
 
-            for (Subject subject : allSubject)
-                for (Object object : allObject) {
-                    object.pass(subject);
-                    if (!subject.isAlive()) {
-                        break;
+        if (!(input_answer == 0)) {
+            String[][] array = new String[input_answer][input_answer];
+            if (input_answer == 4) {
+                for (int i = 0; i < array.length; i++) {
+                    for (int j = 0; j < array[i].length; j++) {
+                        array[i][j] = String.valueOf(i + j);
+                        System.out.print(array[i][j] + " ");
+                        // Вывод значений массива в одну строку с пробелом
                     }
+                    System.out.print("\r\n"); // Переход на новую строку для мультиплатформенных приложений WIn/bsd/HTML
                 }
+                System.out.print("\r\n");
+
+                System.out.println("Правильный массив");
+                changeToIntAndSum(array);
+
+                System.out.println("\n Не правильный массив (not a numbers)");
+                array[random.nextInt(input_answer)][random.nextInt(input_answer)] = "Seven";
+                try {
+                    changeToIntAndSum(array);
+                } catch (MyArrayDataException e) {
+                   // e.printStackTrace();
+                    print("please move forward");
+                    System.exit(-1);
+                }
+            } else
+                System.out.println("Не правильный массив (error of correct size)");
+            try {
+                changeToIntAndSum(array);
+            } catch (MyArraySizeException e) {
+                //e.printStackTrace();
+                print("please move forward");
+            }
         }
         print("Прощай - хорошего дня!");
     } //конец main
 
-    public static String RandomName(String SurnamePrefix) {
-        char[] chars = "abcdefghijklmnopqrstuvwxyz".toCharArray();
-        int NameLength = (2 + random.nextInt(4));
-        StringBuilder sb = new StringBuilder(NameLength); //создаем строку
-        for (int i = 0; i < NameLength; i++) {
-            char c = chars[random.nextInt(chars.length)];
-            sb.append(c); //генерируем строку из случайных букв
+    private static void changeToIntAndSum(String[][] array) throws MyArraySizeException, MyArrayDataException {
+        int Sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                if (array.length != 4 && array[i].length != 4) {
+                    print(("Размер массива != [4][4]. Выбранный массив: [" + array.length + "][" + array[i].length + "]."));
+                    //System.exit(4);
+                    throw new MyArraySizeException("Размер массива != [4][4]. Выбранный массив: [" + array.length + "][" + array[i].length + "].");
+                }
+                try {
+                    Integer.parseInt(array[i][j]);
+                } catch (NumberFormatException e) {
+                    print("На вертикале " + i + ", и горизонтали " + j + " не число, а - " + array[i][j] + "\n");
+                   // System.exit(5);
+                    throw new MyArrayDataException("На вертикале " + i + ", и горизонтали " + j + " не число " + array[i][j] + "\n");
+                }
+                Sum += Integer.parseInt(array[j][j]);
+            }
         }
-        return sb.substring(0, 1).toUpperCase() + sb.substring(1) + SurnamePrefix; //выводим строку с Заглавной 1 буквой
+        System.out.println("Сумма = " + Sum);
+    }
+} //конец LessonMain
+
+class MyArraySizeException extends Exception {
+    public MyArraySizeException(String message) {
+        super(message);
     }
 }
 
+class MyArrayDataException extends NumberFormatException {
+    public MyArrayDataException(String s) {
+        super(s);
+    }
+}
