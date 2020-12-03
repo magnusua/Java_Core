48cfd534644fb52b8f2c143b7102312fc8c5c97f
magnusua <uscmagnus@gmail.com>
2020-12-03 23:03:53 +0200

    1. Напишите метод, на вход которого подается двумерный строковый массив размером 4х4, при подаче массива другого размера необходимо бросить исключение MyArraySizeException. 2. Далее метод должен пройтись по всем элементам массива, преобразовать в int, и просуммировать. Если в каком-то элементе массива преобразование не удалось (например, в ячейке лежит символ или текст вместо числа), должно быть брошено исключение MyArrayDataException – с детализацией, в какой именно ячейке лежат неверные данные. 3. В методе main() вызвать полученный метод, обработать возможные исключения MySizeArrayException и MyArrayDataException и вывести результат расчета.

--- src/main/java/ru/geekbrains/Object.java
+++
@@ -1,5 +0,0 @@
-package ru.geekbrains;
-import ru.geekbrains.Subject;
-public interface Object {
-    void pass(Subject subject);
-}
--- src/main/java/ru/geekbrains/Subject.java
+++
@@ -1,9 +0,0 @@
-package ru.geekbrains;
-
-public interface Subject {
-    public void run(int distance);
-
-    public void heightJump(int height);
-
-    public boolean isAlive();
-}
