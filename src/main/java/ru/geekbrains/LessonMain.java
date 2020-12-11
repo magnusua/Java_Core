ed4217f2f92e6fa561853325d6938cb6c05a7ff4
magnusua <uscmagnus@gmail.com>
2020-12-11 14:21:04 +0200

    1. Создать окно для клиентской части чата: большое текстовое поле для отображения переписки в центре окна. Однострочное текстовое поле для ввода сообщений и кнопка для отсылки сообщений на нижней панели. Сообщение должно отсылаться либо по нажатию кнопки на форме, либо по нажатию кнопки Enter. При «отсылке» сообщение перекидывается из нижнего поля в центральное.

--- src/main/java/ru/geekbrains/LessonMain.java
+++ src/main/java/ru/geekbrains/LessonMain.java
@@ -1,86 +1,67 @@
 package ru.geekbrains;
 
-import java.util.*;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
 
 public class LessonMain {
 
     public static void main(String[] args) { // начало main
-        System.out.println("1");
-//        Задание 1
-        String[] array = {"ache", "slow", "torn", "slum", "boom", "rival", "wrong",
-                "cholera", "revenge", "arrogant", "papa", "book", "home", "ache", "cars",
-                "jolly", "sugar", "friend", "book", "mother", "father", "bloomiest", "ache"};
-        //уникальные и отсортированные элементы элементы
-        Set<String> arrayUnique = new TreeSet<>(Arrays.asList(array));
-        System.out.println(arrayUnique);
-        //подсчитать сколько раз встречаются элементы
-        Map<String, Integer> wordInArrayRepeat = new HashMap<>();
-        for (String word : array) {
-            if (wordInArrayRepeat.containsKey(word)) {
-                wordInArrayRepeat.put(word, wordInArrayRepeat.get(word) + 1);
-            } else {
-                wordInArrayRepeat.put(word, 1);
-            }
-        }
-        // сортировать по comparingByKey() or comparingByValue() и распечатать
-        wordInArrayRepeat.entrySet()
-                .stream()
-                .sorted(Map.Entry.comparingByKey())
-                .forEach(System.out::println);
+        new Main();
 
-        System.out.println("2");
-//        Задание 2
-        PhoneDirectory PhoneDirectory = new PhoneDirectory();
-        Random random = new Random();
-        for (String txt : array) {
-            String name = txt.substring(0, 1).toUpperCase() + txt.substring(1) + "tix";
-            String randomNumber = "+780" + (100000000 + random.nextInt(899999999));
 
-            PhoneDirectory.add(name, randomNumber);
-        }
-
-        PhoneDirectory.Print();
-
-        try {
-            System.out.println(PhoneDirectory.get("Lee"));
-            System.out.println(PhoneDirectory.get("Booktix"));
-            System.out.println(PhoneDirectory.get("Rivaltix"));
-        } catch (Exception e) {
-            System.out.println("Please move forward");
-            System.exit(-1);
-        }
     } //конец main
 } //конец LessonMain
 
-class PhoneDirectory {
-    private final Map<String, ArrayList<String>> phoneBook = new TreeMap<>();
+class Main extends JFrame { //класс
 
-    public void add(String F, String phoneNumber) {
-        ArrayList<String> NumberList = new ArrayList<>();
-        if (phoneBook.containsKey(F)) {
-            NumberList = phoneBook.get(F);
-            NumberList.add(phoneNumber);
-            System.out.println("Для " + F + " добавлен номер: " + phoneNumber);
-        } else {
-            NumberList.add(phoneNumber);
-            phoneBook.put(F, NumberList);
-            System.out.println("Добавлен " + F + ": " + phoneNumber);
-        }
-    }
+    private static final int Size = 600;
 
-    public String get(String F) {
-        String value = String.valueOf(phoneBook.get(F));
-        if ((phoneBook.containsKey(F)) && (value != null)) {
-            return "Для " + F + " записаны номера: " + phoneBook.get(F);
-        } else {
-            return "Для " + F + " не записаны номера";
-        }
-    }
 
-    public void Print() { //печать справочника
-        phoneBook.entrySet()
-                .stream()
-                .sorted(Map.Entry.comparingByKey())
-                .forEach(System.out::println);
-    }
-}
+    private final JTextArea log;
+  //  private final JPanel panelTop;
+    private final JPanel panelBottom;
+    private final JTextField textFieldMessage;
+    private final JButton buttonSend;
+
+    public Main() { //конструктор
+        log = new JTextArea();
+        panelBottom = new JPanel(new BorderLayout());
+        textFieldMessage = new JTextField();
+        buttonSend = new JButton("Send");
+
+        //настройки окна
+        setTitle("Чат");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setSize(Size, Size);
+        setLocationRelativeTo(null); //выравнивание по центру
+        setResizable(false);
+
+        JScrollPane scrollLog = new JScrollPane(log);
+
+        buttonSend.addActionListener(e -> {
+            if (!textFieldMessage.getText().equals("")) {
+                log.append(textFieldMessage.getText() + "\n");
+                textFieldMessage.setText("");
+            }
+        });
+
+        textFieldMessage.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if (!textFieldMessage.getText().equals("") && e.getKeyCode() == KeyEvent.VK_ENTER) {
+                    log.append(textFieldMessage.getText() + "\n");
+                    textFieldMessage.setText("");
+                }
+            }
+        });
+
+        log.setEditable(false);
+        panelBottom.add(textFieldMessage, BorderLayout.CENTER);
+        panelBottom.add(buttonSend, BorderLayout.EAST);
+        add(scrollLog, BorderLayout.CENTER);
+        add(panelBottom, BorderLayout.SOUTH);
+        setVisible(true);
+    }}
