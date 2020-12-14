797902049850f1b81367c3969a87350ee8fb9677
magnusua <uscmagnus@gmail.com>
2020-12-15 00:23:21 +0200

    Проход по массиву и вычисление новых значений

--- src/main/java/ru/geekbrains/LessonMain.java
+++ src/main/java/ru/geekbrains/LessonMain.java
@@ -1,67 +1,70 @@
 package ru.geekbrains;
 
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
+import java.util.Arrays;
+import java.util.Random;
 
+import static java.lang.System.*;
 
 public class LessonMain {
 
     public static void main(String[] args) { // начало main
-        new Main();
+        Random random = new Random();
+        int size = 100000000 + random.nextInt(10000000);
+        System.out.println("Размер массива = "+ size);
+        Thread firstThread = new Thread(() -> {
+            out.println("Проход по массиву и вычисление новых значений: "+Method1(size));
+        });
+        firstThread.start();
+
+        Thread secondThread = new Thread(() -> {
+            out.println("Разбивка массива на два, в двух потоках высчитать новые значения и потом склеить обратно в массив: "+Method2(size));
+        });
+        secondThread.start();
+
+
 
 
     } //конец main
+
+    private static long Method1(int size) {
+        float[] array = new float[size];
+        Arrays.fill(array, 1);
+        long a = currentTimeMillis();
+        fil(array);
+        return currentTimeMillis() - a;
+    }
+
+    private static long Method2(int size) {
+        float[] array = new float[size];
+        Arrays.fill(array, 1);
+        long a = currentTimeMillis();
+        float[] leftArray, rightArray;
+        if (array.length % 2 == 0) {
+            leftArray = Arrays.copyOfRange(array, 0, array.length / 2);
+            rightArray = Arrays.copyOfRange(array, array.length / 2, array.length);
+        } else {
+            leftArray = Arrays.copyOfRange(array, 0, (array.length + 1) / 2);
+            rightArray = Arrays.copyOfRange(array, (array.length + 1) / 2, array.length);
+        }
+        Thread firstThread = new Thread(() -> fil(leftArray));
+        firstThread.start();
+        Thread secondThread = new Thread(() -> fil(rightArray));
+        secondThread.start();
+
+        try {
+            firstThread.join();
+            secondThread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        arraycopy(leftArray, 0, array, 0, leftArray.length);
+        arraycopy(rightArray, 0, array, leftArray.length, rightArray.length);
+        return currentTimeMillis() - a;
+    }
+
+    private static void fil(float[] array) {
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (float) (array[i] * Math.sin(0.2f + (float) i / 5) * Math.cos(0.2f + (float) i / 5) * Math.cos(0.4f + (float) i / 2));
+        }
+    }
 } //конец LessonMain
-
-class Main extends JFrame { //класс
-
-    private static final int Size = 600;
-
-
-    private final JTextArea log;
-  //  private final JPanel panelTop;
-    private final JPanel panelBottom;
-    private final JTextField textFieldMessage;
-    private final JButton buttonSend;
-
-    public Main() { //конструктор
-        log = new JTextArea();
-        panelBottom = new JPanel(new BorderLayout());
-        textFieldMessage = new JTextField();
-        buttonSend = new JButton("Send");
-
-        //настройки окна
-        setTitle("Чат");
-        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        setSize(Size, Size);
-        setLocationRelativeTo(null); //выравнивание по центру
-        setResizable(false);
-
-        JScrollPane scrollLog = new JScrollPane(log);
-
-        buttonSend.addActionListener(e -> {
-            if (!textFieldMessage.getText().equals("")) {
-                log.append(textFieldMessage.getText() + "\n");
-                textFieldMessage.setText("");
-            }
-        });
-
-        textFieldMessage.addKeyListener(new KeyAdapter() {
-            @Override
-            public void keyPressed(KeyEvent e) {
-                if (!textFieldMessage.getText().equals("") && e.getKeyCode() == KeyEvent.VK_ENTER) {
-                    log.append(textFieldMessage.getText() + "\n");
-                    textFieldMessage.setText("");
-                }
-            }
-        });
-
-        log.setEditable(false);
-        panelBottom.add(textFieldMessage, BorderLayout.CENTER);
-        panelBottom.add(buttonSend, BorderLayout.EAST);
-        add(scrollLog, BorderLayout.CENTER);
-        add(panelBottom, BorderLayout.SOUTH);
-        setVisible(true);
-    }}
